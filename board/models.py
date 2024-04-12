9f6ee99d0c3e4d270c00ee9a2bd226a0aea60736
G0LDF0X <astnv31@gmail.com>
2024-04-12 17:14:38 +0900

    Board project

---
+++ board/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+class Comment(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    comment = models.TextField()
